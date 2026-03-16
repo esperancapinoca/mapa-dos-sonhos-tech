139fc66975cd707765a1c919f8d82dcf6c9f9b4e
Esperança Pinoca <esperancapinocaof@gmail.com>
2026-03-16 17:07:38 +0200

    Fix import path in urls.py

--- backend/mapa_de_sonhos_tech_backend/urls.py
+++ backend/mapa_de_sonhos_tech_backend/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from backend.dreams.views import home
+from dreams.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
